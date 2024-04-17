01d3ec53f906e3671c29d70dcd83c92c0b01ec8f
Kumar <tarekum99@gmail.com>
2024-04-17 16:27:16 +0530

    Added

---
+++ Basics.R
@@ -0,0 +1,13 @@
+
+
+# Vectors in R ------------------------------------------------------------
+
+MyFirstVector <- c(3,4,5)
+view(MyFirstVector)
+
+# functions in R ----------------------------------------------------------
+
+rnorm()
+
+rep(MyFirstVector,each=10)
+
