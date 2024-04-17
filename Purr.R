8a3079349c3be91c6ba4068357bc22fe3cf58b73
Kumar <tarekum99@gmail.com>
2024-04-17 19:30:40 +0530

    Added all filest

--- Purr.R
+++ Purr.R
@@ -19,5 +19,69 @@ view(DF)
 
 faithful_chr <- faithful %>% map_df(as.character)
 glimpse(faithful_chr)
-View
+View(faithful_chr)
+
+
+filtered_faithful <- faithful %>%
+  filter(eruptions > 3 & eruptions < 5)# Filter eruptions longer than 3 minutes and shorter than 5 minutes
+glimpse(filtered_faithful)
+
+avg_waiting <- faithful %>%  # the average waiting time
+  summarize(avg_waiting = mean(waiting))
+view(avg_waiting)
+
+print(filtered_faithful)
+print(avg_waiting)
+
+faithful_long <- faithful %>%
+  pivot_longer(cols = c(eruptions, waiting), names_to = "variable", values_to = "value")  # Pivot 'faithful' data to a longer format
+
+print(faithful_long)
+
+
+# Create a scatter plot of eruptions vs waiting times
+plot <- ggplot(faithful, aes(x = eruptions, y = waiting)) +
+  geom_point() +
+  labs(title = "Eruptions vs. Waiting Time",
+       x = "Eruption Duration (minutes)",
+       y = "Waiting Time (minutes)") +
+  theme_minimal()
+
+print(plot)
+
+
+grouped_faithful <- faithful %>%       # Find the eruption duration and waiting times grouped by minutes
+  mutate(eruptions_min = floor(eruptions),
+         waiting_min = floor(waiting)) %>%
+  group_by(eruptions_min, waiting_min) %>%
+  summarize(count = n()) %>%
+  arrange(desc(count))
+
+print(grouped_faithful)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
